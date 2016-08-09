abad9133a386baaa1a09396c34f52ca2886435cc
Laura <laura@Lauras-MacBook-Pro.local>
2016-08-09 23:30:58 +0100

    delete not used classes

--- src/main/java/com/lordkote/gmail/ServiceConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.lordkote.gmail;
-
-import com.manywho.sdk.services.configuration.Configuration;
-
-/**
- * Created by Jose on 02/08/2016.
- */
-public class ServiceConfiguration implements Configuration {
-}
